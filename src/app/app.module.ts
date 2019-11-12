5b7ea8836ade04e893a868d0dbdf6100071923ef
denizburduroglu <denizburduroglu@gmail.com>
2019-11-12 17:14:02 -0500

    Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatSelectModule, MatNativeDateModule, MatDatepickerModule, MatInputModule, MatFormFieldModule, MatSortModule, MatToolbarModule, MatTableModule, MatPaginatorModule, MatMenuModule, MatButtonModule, MatIconModule, MatCardModule } from '@angular/material';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 
+import { AppComponent } from './app.component';
 import { TableComponent } from './table';
 import { AddTaskComponent } from './addTask';
 import { LoginComponent } from './login/login.component';
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+import { Post } from './post';
+import { Observable } from 'rxjs';
+import { map, retry, catchError } from 'rxjs/operators';
 
 @Injectable()
 @Component({
@@ -9,9 +12,33 @@ import { HttpClient } from '@angular/common/http';
 })
 export class LoginComponent implements OnInit {
 
+  readonly ROOT_URL = 'https://jsonplaceholder.typicode.com';
+
+  posts: Observable<any>;
+  newPost: Observable<any>;
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
+
   }
 
+  getPosts() {
+    let headers = new HttpHeaders().set('Authorization', 'auth-token');
+    // let params = new HttpParams().set('userId', '1');
+    this.posts = this.http.get(this.ROOT_URL + '/posts', { headers })
+  }
+
+  createPost() {
+    const data: Post = {
+      id: null,
+      userId: 23,
+      title: 'My New Post',
+      body: 'Hello World!'
+    }
+
+    this.newPost = this.http.post<Post>(this.ROOT_URL + '/foo', data).pipe(map(post => post.title)).pipe(retry(3)).pipe(catchError(err => {
+      console.log(err);
+      return Observable;
+    }));
+  }
 }
